3bfe9bc8b3eda7b11c6c70dce6904e8d9426a243
kamal336 <akterkamal336@gmail.com>
2021-11-13 22:56:57 +0600

    delete item

--- index.js
+++ index.js
@@ -127,6 +127,15 @@ async function run() {
       console.log(result);
     })
 
+
+    // delete event 
+    app.delete('/orders/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
+      res.json(result);
+    })
+    
     } finally {
     //   await client.close();
     }
